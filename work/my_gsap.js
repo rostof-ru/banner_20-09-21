70563ef27f36cba54195161488a8bb17e425f8b2
rostof.ru <melnikov_v@rostof.ru>
2020-09-25 10:51:40 +0300

    multi-run and cikle

--- work/my_gsap.js
+++ work/my_gsap.js
@@ -1,12 +1,5 @@
 
-// alert("Привет, МИР !!! GSAP2");
-
-// window.onload = function(){
-//     var logo = document.getElementById("my-logo");
-//     TweenLite.from(logo, 1, {left:"632px"});
-// }
-
-window.onload = function(){
+function animeMobile() {
     const logo = document.getElementById("my-logo");
     const telefon = document.getElementById("my-telefon");
     const restoran = document.getElementById("my-restoran");
@@ -25,41 +18,51 @@ window.onload = function(){
     var myTime = 0.9;
     var myRotate = -160;
     var tl = new TimelineLite();
+    let home_ad_brend_560 = document.querySelector('.home_ad_brend_560');
 
-    // TweenLite.from(logo, 1, {left:"632px", ease:Back.easeOut});
-    // TweenLite.from(textChars[0], 2, {left:"632px", delay:delayValue, ease:easeOut});
-    // TweenLite.from(textChars[1], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[2], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[3], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[4], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[5], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[6], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[7], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[8], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[9], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[10], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(textChars[11], 2, {left:"632px", delay:(delayValue += delayPlus), ease:easeOut});
-    // TweenLite.from(restoran, 4, {opacity: 0, ease:Back.easeOut});
-    // TweenLite.from(sad, 3, {opacity: 0, ease:Back.easeOut});
-    // TweenLite.from(my_overlay, 4, {opacity: 0, ease:Back.easeOut});
-    // TweenLite.from(overlay, 4, {opacity: 0, ease:Back.easeOut});
-    // TweenLite.staggerFrom( textChars, 0.5, { opacity: 0, rotation: -180, ease: easeOut }, 0.02);
+    if (screenMode(home_ad_brend_560, "display") != "none") {
+        tl.from(logo, 1, {left:"632px", ease:Back.easeOut})
+        .staggerFrom(textChars, myTime, {top:"-135vw", rotation: myRotate, autoAlpha: alpha, ease:easeOut}, 0.2);
+        console.log("Выполнил функцию \"animeMobile\"");
+    } else console.log("Это НЕ ПОНЯТНО ЧТО");
+        
+    // tl.from(logo, 1, {left:"632px", ease:Back.easeOut})
+    // .staggerFrom(textChars, myTime, {top:"-135vw", rotation: myRotate, autoAlpha: alpha, ease:easeOut}, 0.2);
+    // console.log("Выполнил функцию \"animeMobile\"");
+}
 
-    // tl.from(logo, 1, {left:"632px", ease:Back.easeOut});
-    // tl.from(textChars[0], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[1], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[2], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[3], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[4], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[5], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[6], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[7], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[8], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[9], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[10], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.from(textChars[11], 2, {left:"132vw", ease:easeOut}, delayMinus);
-    // tl.staggerFrom(textChars, myTime, {left:"132vw", top:"-35vw", rotation: myRotate, autoAlpha: alpha, ease:easeOut}, 0.2);
-    tl.from(logo, 1, {left:"632px", ease:Back.easeOut})
-    .staggerFrom(textChars, myTime, {top:"-135vw", rotation: myRotate, autoAlpha: alpha, ease:easeOut}, 0.2);
+function animeDesktop() {
+    console.log("Выполнил функцию \"animeDesktop\"");
+}
+
+function screenMode (selector, property) {
+    return window.getComputedStyle(selector,null).getPropertyValue(property);
+}
+
+window.onload = function(){
+    let home_ad_brend_1080 = document.querySelector('.home_ad_brend_1080');
+    let home_ad_brend_560 = document.querySelector('.home_ad_brend_560');
+ 
+    console.log(screenMode(home_ad_brend_1080, "display"));
+    console.log(screenMode(home_ad_brend_560, "display"));
+
+    if (screenMode(home_ad_brend_1080, "display") != "none") {
+        console.log("Это широкий экран");
+        animeDesktop();
+    } else if (screenMode(home_ad_brend_560, "display") != "none") {
+        console.log("Это узкий, MODILE экран");
+        animeMobile();
+        // setTimeout(animeMobile, 7000);
+        // setInterval(() => {
+        //     console.log("Повторюшка");
+        // }, 3000);
+        setInterval(() => {
+            animeMobile();
+        }, 6000);
+    } else console.log("Это НЕ ПОНЯТНО ЧТО");
+
+    console.log("Закрузил");
+    console.log(screenMode(home_ad_brend_560, "display"));
+ 
 }
 
